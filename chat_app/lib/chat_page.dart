89f5914910007ab21bf67a4afdc295d477f24eb5
Mustafa Assaf <mustafaassaf@cloud.upwork.com>
2025-03-14 11:45:32 +0200

    Placements and alignments

--- chat_app/lib/chat_page.dart
+++ chat_app/lib/chat_page.dart
@@ -19,60 +19,69 @@ class ChatPage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          Container(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text("Hi this is your message!", style: TextStyle(fontSize: 20, color: Colors.white),),
-                Image.network("https://shorturl.at/PkTiO", height: 150),
-              ],
-            ),
-            padding: EdgeInsets.all(24),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomLeft: Radius.circular(12),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Hi this is your message!", style: TextStyle(fontSize: 20, color: Colors.white),),
+                  Image.network("https://shorturl.at/PkTiO", height: 120),
+                ],
+              ),
+              padding: EdgeInsets.all(24),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: Radius.circular(12),
+                ),
               ),
             ),
           ),
-          Container(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text("Hi this is your message!", style: TextStyle(fontSize: 20, color: Colors.white),),
-                Image.network("https://shorturl.at/PkTiO", height: 150),
-              ],
-            ),
-            padding: EdgeInsets.all(24),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomLeft: Radius.circular(12),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Hi this is your message!", style: TextStyle(fontSize: 20, color: Colors.white),),
+                  Image.network("https://shorturl.at/PkTiO", height: 120),
+                ],
+              ),
+              padding: EdgeInsets.all(24),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: Radius.circular(12),
+                ),
               ),
             ),
           ),
-          Container(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text("Hi this is your message!", style: TextStyle(fontSize: 20, color: Colors.white),),
-                Image.network("https://shorturl.at/PkTiO", height: 150),
-              ],
-            ),
-            padding: EdgeInsets.all(24),
-            margin: EdgeInsets.all(50),
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12),
-                bottomLeft: Radius.circular(12),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Hi this is your message!", style: TextStyle(fontSize: 20, color: Colors.white),),
+                  Image.network("https://shorturl.at/PkTiO", height: 120),
+                ],
+              ),
+              padding: EdgeInsets.all(24),
+              margin: EdgeInsets.all(50),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: Radius.circular(12),
+                ),
               ),
             ),
           ),
@@ -85,6 +94,7 @@ class ChatPage extends StatelessWidget {
               ),
             ),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(onPressed: (){}, icon: Icon(Icons.add), color: Colors.white,),
                 IconButton(onPressed: (){}, icon: Icon(Icons.send), color: Colors.white,),
