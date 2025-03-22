82b185b6abaf328aa475d27a13e1eab0c7d62100
Mustafa Assaf <mustafaassaf@cloud.upwork.com>
2025-03-23 01:55:12 +0200

    customize colors, typography, button styles, and more using ThemeData.

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -9,6 +9,7 @@ void main() {
 class ChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //TODO: customize colors, typography, button styles, and more using ThemeData.
     return MaterialApp(title: 'Flutter Demo', home: HomePage());
   }
 }
