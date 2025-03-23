cd807df8c0eb7e6975db4f7b0ee45af9e6a0e0b1
Mustafa Assaf <mustafaassaf@cloud.upwork.com>
2025-03-23 22:28:38 +0200

    adding buttons

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,59 +9,81 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final Uri _url = Uri.parse('https://flutter.dev');
+
   bool isButtonEnabled = true;
+
+  void Function()? loginUser() {
+    return isButtonEnabled ? () => print('Login Successful') : null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.brown,
-                fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Let\'s sign you in!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              "Welcome back!\n You've been missed!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
+              Text(
+                "Welcome back!\n You've been missed!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            Image.network("https://shorturl.at/RqyFD", height: 200),
+              Image.network("https://shorturl.at/RqyFD", height: 200),
 
-            SizedBox(
-              width: double.infinity, // Full width
-              child: ElevatedButton(
-                onPressed:
-                    isButtonEnabled ? () => print('Login Successful') : null,
-                child: Text(
-                  'Click Me',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: loginUser,
+                  child: Text(
+                    'Click Me',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-            ),
 
-            OutlinedButton(
-              onPressed: () {},
-              child: FlutterLogo(), // Using a logo instead of text
-            ),
+              OutlinedButton(
+                onPressed: () {},
+                child: FlutterLogo(), // Using a logo instead of text
+              ),
 
-            TextButton(onPressed: () {}, child: Text('Go to Website')),
-          ],
+              TextButton(
+                onPressed: _launchUrl,
+                child: Text(
+                  'Go to Website',
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> _launchUrl() async {
+    if (!await launchUrl(_url)) {
+      throw Exception('Could not launch $_url');
+    }
   }
 }
