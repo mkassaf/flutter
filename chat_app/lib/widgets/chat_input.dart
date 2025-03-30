473636390bad7456af0a858113b8031e3098342b
Mustafa Assaf <mustafaassaf@cloud.upwork.com>
2025-03-30 22:54:16 +0200

    Creating multiline text fields

--- chat_app/lib/widgets/chat_input.dart
+++ chat_app/lib/widgets/chat_input.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ChatInput extends StatelessWidget {
-  const ChatInput({super.key});
+  ChatInput({super.key});
+
+  final chatMessageController = TextEditingController();
+
+  void onSendButtonPressed() {
+    print("Message sent: ${chatMessageController.text}");
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
       decoration: BoxDecoration(
         color: Colors.black,
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
@@ -19,9 +24,23 @@ class ChatInput extends StatelessWidget {
             icon: Icon(Icons.add),
             color: Colors.white,
           ),
-          //TODO: Add multiline text input
+          Expanded(
+            child: TextField(
+              controller: chatMessageController,
+              style: TextStyle(color: Colors.white),
+              keyboardType: TextInputType.multiline,
+              minLines: 1,
+              maxLines: 5,
+              textCapitalization: TextCapitalization.sentences,
+              decoration: InputDecoration(
+                hintText: 'Type a message',
+                hintStyle: TextStyle(color: Colors.blueGrey),
+                border: InputBorder.none,
+              ),
+            ),
+          ),
           IconButton(
-            onPressed: () {},
+            onPressed: onSendButtonPressed,
             icon: Icon(Icons.send),
             color: Colors.white,
           ),
