40243758ee4e724e688d545a3f2d861b7544dc4f
Mustafa Assaf <mustafaassaf@cloud.upwork.com>
2025-03-31 00:07:54 +0200

    Passing data with navigation

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -21,6 +21,7 @@ class _LoginPageState extends State<LoginPage> {
 
   void loginUser() {
     if (_formKey.currentContext != null && _formKey.currentState!.validate()) {
+      //TODO: Add Named Routes instead of anonymous routes
       Navigator.push(
         context,
         MaterialPageRoute(
