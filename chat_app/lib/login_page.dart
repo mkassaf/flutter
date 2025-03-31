845f9c2beb108b8fa3970a5b596a5611a44bf47a
Mustafa Assaf <mustafaassaf@cloud.upwork.com>
2025-03-31 22:22:30 +0200

    5. Improve the login page using SingleChildScrollView

--- chat_app/lib/login_page.dart
+++ chat_app/lib/login_page.dart
@@ -39,133 +39,135 @@ class _LoginPageState extends State<LoginPage> {
       body: Padding(
         padding: const EdgeInsets.all(24),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Let\'s sign you in!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                "Welcome back!\n You've been missed!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              Image.network("https://shorturl.at/RqyFD", height: 200),
-
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    // Username field
-                    TextFormField(
-                      validator: (value) {
-                        if (value != null && value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        if (value!.length < 5) {
-                          return 'Username must be at least 5 characters long';
-                        }
-                        return null;
-                      },
-                      controller: userNameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Add your username",
-                        hintStyle: TextStyle(color: Colors.blueGrey),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 24,
-                    ), // Adds space between form fields and buttons
-                    // Password field
-                    TextFormField(
-                      validator: (value) {
-                        if (value != null && value.isEmpty) {
-                          return 'Password cannot be empty';
-                        }
-                        return null;
-                      },
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Type your password",
-                        hintStyle: TextStyle(color: Colors.blueGrey),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ), // Adds space between form fields and buttons
-
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.resolveWith((states) {
-                    // Check if the button is pressed or hovered
-                    if (states.contains(WidgetState.pressed)) {
-                      return Colors.blue; // Color when pressed
-                    } else if (states.contains(WidgetState.hovered)) {
-                      return Colors.green; // Color when hovered
-                    } else if (states.contains(WidgetState.disabled)) {
-                      return Colors.grey; // Color when disabled
-                    }
-                    return Colors.green; // Default color
-                  }),
-                ),
-                onPressed: loginUser,
-                child: Text(
-                  'Login',
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Let\'s sign you in!',
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
+                    fontSize: 30,
                     color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
+                Text(
+                  "Welcome back!\n You've been missed!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Image.network("https://shorturl.at/RqyFD", height: 200),
 
-              OutlinedButton(
-                onPressed: () {},
-                child: FlutterLogo(), // Using a logo instead of text
-              ),
-
-              Material(
-                child: InkWell(
-                  splashColor: Colors.red, // Adds a red ripple effect on tap
-                  onDoubleTap: () {
-                    print('Double Tap Detected');
-                  },
-                  onLongPress: () {
-                    print('Long Press Detected');
-                  },
-                  onTap: _launchUrl,
+                Form(
+                  key: _formKey,
                   child: Column(
                     children: [
-                      Text(
-                        "Find more about Flutter",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
+                      // Username field
+                      TextFormField(
+                        validator: (value) {
+                          if (value != null && value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          if (value!.length < 5) {
+                            return 'Username must be at least 5 characters long';
+                          }
+                          return null;
+                        },
+                        controller: userNameController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Add your username",
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ), // Adds space between form fields and buttons
+                      // Password field
+                      TextFormField(
+                        validator: (value) {
+                          if (value != null && value.isEmpty) {
+                            return 'Password cannot be empty';
+                          }
+                          return null;
+                        },
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Type your password",
+                          hintStyle: TextStyle(color: Colors.blueGrey),
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 24,
+                ), // Adds space between form fields and buttons
+
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.resolveWith((states) {
+                      // Check if the button is pressed or hovered
+                      if (states.contains(WidgetState.pressed)) {
+                        return Colors.blue; // Color when pressed
+                      } else if (states.contains(WidgetState.hovered)) {
+                        return Colors.green; // Color when hovered
+                      } else if (states.contains(WidgetState.disabled)) {
+                        return Colors.grey; // Color when disabled
+                      }
+                      return Colors.green; // Default color
+                    }),
+                  ),
+                  onPressed: loginUser,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+
+                OutlinedButton(
+                  onPressed: () {},
+                  child: FlutterLogo(), // Using a logo instead of text
+                ),
+
+                Material(
+                  child: InkWell(
+                    splashColor: Colors.red, // Adds a red ripple effect on tap
+                    onDoubleTap: () {
+                      print('Double Tap Detected');
+                    },
+                    onLongPress: () {
+                      print('Long Press Detected');
+                    },
+                    onTap: _launchUrl,
+                    child: Column(
+                      children: [
+                        Text(
+                          "Find more about Flutter",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
